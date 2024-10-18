75f398316acf6a0fc7909e06c24c2a41b2530ef1
ruslanhq <h.ruslan97@gmail.com>
2024-10-18 10:42:57 +0600

    implement postgres

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,11 @@
 echo "Performing database migrations..."
 python3 manage.py migrate
 
-echo "Creating superuser..."
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+if [ -z "$(echo "from django.contrib.auth.models import User; print(User.objects.filter(is_superuser=True).exists())" | python manage.py shell)" ]; then
+    echo "Creating superuser..."
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+else
+    echo "Superuser already exists"
+fi
 
 exec gunicorn src.config.wsgi:application --bind 0.0.0.0:8000
--- src/config/settings/local.py
+++ src/config/settings/local.py
@@ -17,10 +17,21 @@ ALLOWED_HOSTS = ["*"]
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', '12345678'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
